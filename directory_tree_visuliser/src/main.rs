6bcb47c7c6b068f29bd9d3e3e548db4bc831901c
baltej223 <baltej963@gmail.com>
2026-01-10 09:56:29 +0530

    Recursively traverses the filesystems and dumps found files

---
+++ directory_tree_visuliser/src/main.rs
@@ -0,0 +1,52 @@
+use std::fs;
+use std::path::Path;
+use std::env::current_dir as cwd;
+
+struct FilesInFolder{
+    is_dir:bool,
+    pathIs:std::path::PathBuf,
+    file_name:std::ffi::OsString,
+}
+
+fn read_folder(reative_path:&std::path::Path) -> Vec<FilesInFolder>{
+    let mut file_vec : Vec<FilesInFolder>  = Vec::new();
+    let dir_con = fs::read_dir(&reative_path).expect("1 Here.");
+    
+    for i in dir_con {
+        let cur_fl = i.expect("2 Here");
+        let file_type = cur_fl.file_type().expect("Failed to get file type");
+        let is_dir = file_type.is_dir();
+        let file_name = cur_fl.file_name();
+
+        let file_struct =  FilesInFolder { 
+            is_dir:is_dir,
+            pathIs:cur_fl.path(),
+            file_name
+        };
+        // Now pushing this to the vector 
+        let _ = &file_vec.push(file_struct);
+    }
+    file_vec
+}
+
+fn main() {
+    // Get the current working directory 
+    let binding = cwd().expect("Here");
+    let curr_dir = Path::new(&binding);
+
+    recursive_print_insides_of_folder(curr_dir.to_path_buf());
+}
+
+fn recursive_print_insides_of_folder(path_to: std::path::PathBuf){
+    // println!("Path which is being sent to the func is: {}", path_to.display());
+    let this_folder : Vec<FilesInFolder> = read_folder(&path_to);
+
+    for file in this_folder {
+        
+        if !file.is_dir {
+            println!("File at path: {}", file.pathIs.display());
+            continue;
+        }
+        recursive_print_insides_of_folder(file.pathIs);
+    }
+}
