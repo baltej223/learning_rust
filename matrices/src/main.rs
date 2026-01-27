f3593436a0b1a71d561d3e0cfa04ff5dd4e89542
baltej223 <baltej963@gmail.com>
2026-01-27 13:31:40 +0530

    Implied some methods to the Matrix datatype

--- matrices/src/main.rs
+++ matrices/src/main.rs
@@ -1,75 +1,76 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-struct Matrix<T> {
+struct Matrix {
     rows: usize,
     cols: usize,
-    data: Vec<Vec<T>>,
+    data: Vec<Vec<i32>>,
 }
 
-fn construct(data: Vec<Vec<i32>>) -> Matrix<i32> {
-    let rows = data.len();
+impl Matrix {
+    fn construct(data: Vec<Vec<i32>>) -> Matrix {
+        let rows = data.len();
 
-    let cols = data[0].len();
-    for i in 1..data.len() {
-        if data[i].len() != cols {
-            panic!("all the columns should be of same number.");
+        let cols = data[0].len();
+        for i in 1..data.len() {
+            if data[i].len() != cols {
+                panic!("all the columns should be of same number.");
+            }
         }
+        Matrix { rows, cols, data }
     }
-    Matrix { rows, cols, data }
-}
 
-fn print_matrix(matrix: &Matrix<i32>) {
-    for row in 0..matrix.rows {
-        for col in 0..matrix.cols {
-            print!("{} ", matrix.data[row][col]);
+    fn print_matrix(&self) {
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                print!("{} ", self.data[row][col]);
+            }
+            println!(" ");
         }
         println!(" ");
     }
-    println!(" ");
-}
 
-fn construct_zero_matrix(rows: usize, cols: usize) -> Matrix<i32> {
-    let mut zero_vector = Vec::new();
-    for row in 0..rows {
-        let mut inner_zero_vector = Vec::new();
-        for col in 0..cols {
-            inner_zero_vector.push(0);
+    fn construct_zero_matrix(rows: usize, cols: usize) -> Matrix {
+        let mut zero_vector = Vec::new();
+        for row in 0..rows {
+            let mut inner_zero_vector = Vec::new();
+            for col in 0..cols {
+                inner_zero_vector.push(0);
+            }
+            zero_vector.push(inner_zero_vector);
         }
-        zero_vector.push(inner_zero_vector);
+        Matrix::construct(zero_vector)
     }
-    construct(zero_vector)
-}
 
-fn transpose(matrix: Matrix<i32>) -> Matrix<i32> {
-    let rows = matrix.rows;
-    let cols = matrix.cols;
-    let mut transposed_matrix: Matrix<i32> = construct_zero_matrix(rows, cols);
-    for row in 0..matrix.rows {
-        for col in 0..matrix.cols {
-            transposed_matrix.data[row][col] = matrix.data[col][row];
-        }
-    }
-    transposed_matrix
-}
-
-fn construct_identity_matrix(rows: usize, cols: usize) -> Matrix<i32> {
-    let mut zero_matrix = construct_zero_matrix(rows, cols);
-    for row in 0..rows {
-        for col in 0..cols {
-            if row == col {
-                zero_matrix.data[row][col] = 1;
+    fn transpose(&mut self) {
+        let rows = self.rows;
+        let cols = self.cols;
+        let mut transposed_matrix: Matrix = Self::construct_zero_matrix(rows, cols);
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                transposed_matrix.data[row][col] = self.data[col][row];
             }
         }
+        self.data = transposed_matrix.data;
     }
-    zero_matrix
-}
 
+    fn construct_identity_matrix(n: usize) -> Matrix {
+        let mut zero_matrix = Matrix::construct_zero_matrix(n, n);
+        for row in 0..n {
+            for col in 0..n {
+                if row == col {
+                    zero_matrix.data[row][col] = 1;
+                }
+            }
+        }
+        zero_matrix
+    }
+}
 fn main() {
     let vec = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
-    let matrix = construct(vec);
-    print_matrix(&matrix);
-    let transposed_mat = transpose(matrix);
-    print_matrix(&transposed_mat);
-    print_matrix(&construct_identity_matrix(12, 5));
+    let mut matrix = Matrix::construct(vec);
+    matrix.print_matrix();
+    matrix.transpose();
+    matrix.print_matrix();
+    Matrix::construct_identity_matrix(12).print_matrix();
 }
