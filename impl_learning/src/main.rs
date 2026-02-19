56efbba9cf46cef54f24def1296af792d2c4a271
baltej223 <baltej963@gmail.com>
2026-02-20 00:55:13 +0530

    impl

---
+++ impl_learning/src/main.rs
@@ -0,0 +1,16 @@
+struct Complex {
+    real: i32,
+    img: i32,
+}
+
+impl Complex {
+    fn distance(self) {
+        let distance = (((self.real) ^ 2 + (self.img) ^ 2) as f64).sqrt();
+        println!("{}", distance);
+    }
+}
+
+fn main() {
+    let c = Complex { real: 21, img: 32 };
+    c.distance();
+}
